8bf152a2e8b5861f003bd8ee3466e8b05971bc6a
nabeeltaariq <nabeeltaariq@gmail.com>
2021-01-06 16:31:19 +0500

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello netflix clone</h2>
+      <Row title="NETFLIX ORIGINALS"/> 
+      <Row title="TRENDING NOW"/>
+    
     </div>
   );
 }
 
 export default App;
+ 
