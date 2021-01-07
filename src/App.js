06193fbf93de34ea25367dff391ef2d853a75ec4
nabeeltaariq <nabeeltaariq@gmail.com>
2021-01-07 17:20:27 +0500

    styling main page and poster rows

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-
-import './App.css';
-import Row from './Row';
+import './App.css'
+import Row from './Row'
+import React from 'react'
+import requests from './requests'
+import Banner from './Banner'
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello netflix clone</h2>
-      <Row title="NETFLIX ORIGINALS"/> 
-      <Row title="TRENDING NOW"/>
-    
+    <div className='App'>
+      <Banner></Banner>
+      <Row
+        title='NETFLIX ORIGINALS'
+        isLargeRow
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title='Trending' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>
-  );
+  )
 }
 
-export default App;
- 
+export default App
